275a37249c42e740f402d5307fcc483e819bcdfb
github username <github email address>
2018-11-04 01:24:28 +0000

    Done -- unable to get rid of one name error

--- review-question-3.rb
+++ review-question-3.rb
@@ -1,22 +1,81 @@
+require 'pry'
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
 
+class User
+  attr_accessor :name, :comms, :jpgs
+    @@all_users = []
 
+    def initialize(name)
+      @name = name
+      @comms = []
+      @jpgs =[]
+      @@all_users << self
+    end
+
+    def self.all
+      @@all_users
+    end
+
+    def photos
+      Photo.all.select do |photo|
+        photo.user == self
+      end
+    end
+end
+
+
+class Photo
+    attr_accessor :user, :comms
+     @@all = []
+
+     def initialize
+       @comms = []
+       @@all << self
+     end
+
+     def self.all
+       @all
+     end
+
+    def comments
+      @comms
+    end
+
+    def make_comment(user_comment)
+      self.comments << Comment.new(user_comment)
+    end
+end
+
+class Comment
+  attr_accessor :comm, :user, :jpgs
+  @@all = []
+
+  def initialize(comm)
+      @comm = comm
+      @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
+binding.pry
 sandwich_photo = Photo.new
 sophie = User.new("Sophie")
 sandwich_photo.user = sophie
-sandwich_photo.user.name
+sandwich_photo.user.name  #---ok
 # => "Sophie"
-user.photos
+user.photos  #---NameError: undefined local variable or method `user'
 # => [#<Photo:0x00007fae2880b370>]
 
-
-sandwich_photo.comments
+sandwich_photo.comments --ok
 # => []
 
 sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
-sandwich_photo.comments
+sandwich_photo.comments --ok
 # => [#<Comment:0x00007fae28043700>]
 
-Comment.all
+Comment.all --ok
 #=> [#<Comment:0x00007fae28043700>]
